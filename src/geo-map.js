b0a7d7b2f52fc405cbea06cf8c0bffade770363e
LNSY <lindsey.asterius@gmail.com>
2023-12-05 14:30:55 -0800

    added layer lists, etc

--- src/geo-map.js
+++ src/geo-map.js
@@ -1,96 +1,265 @@
-// Importing Mapbox and its geocoder plugin
 import 'https://api.mapbox.com/mapbox-gl-js/v2.9.2/mapbox-gl.js';
 import 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.0/mapbox-gl-geocoder.min.js';
 
-// Importing helper functions
 import { getURLValues, ready } from './helpers.js';
 
-// Defining a custom web component for the GeoMap
 class GeoMapComponent extends HTMLElement {
   constructor() {
     super();
-
-    // Checking if Mapbox is available
-    if (typeof mapboxgl === 'undefined') {
+    if(typeof(mapboxgl) === 'undefined'){
       return console.error('Geo Map component requires Mapbox to work');
     }
+    const URLvalues = getURLValues();
 
-    // Getting Mapbox access token from the attribute
     this.access_token = this.getAttribute('accesstoken');
-    if (this.access_token === null) {
+    if(this.access_token === null){
       return console.error('Geo Map component requires a MapBox access token');
     }
     this.removeAttribute('accesstoken');
     mapboxgl.accessToken = this.access_token;
 
-    // Getting and setting various attributes for map configuration
-    this.styleurl = this.getAttribute('styleurl') || 'mapbox://styles/mapbox/streets-v11';
+    this.styleurl = this.getAttribute('styleurl');
+    if(this.styleurl === null || this.styleurl === ""){
+      console.warn('could not find style url, using the default');
+      this.styleurl = 'mapbox://styles/mapbox/streets-v11';
+    }
     this.removeAttribute('styleurl');
 
-    this.latitude = this.getAttribute('latitude') || 0;
-    this.latitude = getURLValues().latitude || this.latitude;
+    this.latitude = this.getAttribute('latitude');
+    if(this.latitude === null) this.latitude = 0;
+    this.latitude = URLvalues.latitude ? URLvalues.latitude : this.latitude;
 
-    this.longitude = this.getAttribute('longitude') || 0;
-    this.longitude = getURLValues().longitude || this.longitude;
+    this.longitude = this.getAttribute('longitude');
+    if(this.longitude === null) this.longitude = 0;
+    this.longitude = URLvalues.longitude ? URLvalues.longitude : this.longitude;
 
-    this.zoom = this.getAttribute('zoom') || 1;
-    this.zoom = getURLValues().zoom || this.zoom;
+    this.zoom = this.getAttribute('zoom');
+    if(this.zoom === null) this.zoom = 1;
+    this.zoom = URLvalues.zoom ? URLvalues.zoom : this.zoom;
 
-    this.bearing = this.getAttribute('bearing') || 0;
-    this.bearing = getURLValues().bearing || this.bearing;
+    this.bearing = this.getAttribute('bearing');
+    if(this.bearing === null) this.bearing = 0;
+    this.bearing = URLvalues.bearing ? URLvalues.bearing : this.bearing;
 
-    this.pitch = this.getAttribute('pitch') || 0;
-    this.pitch = getURLValues().pitch || this.pitch;
+    this.pitch = this.getAttribute('pitch');
+    if(this.pitch === null) this.pitch = 0;
+    this.pitch = URLvalues.pitch ? URLvalues.pitch : this.pitch;
 
-    this.locked = this.getAttribute('locked') !== null;
+    this.locked = this.getAttribute('locked');
+    if(this.locked === null){
+      this.locked = false;
+    } else {
+      this.locked = true;
+    };
+
+    this.navigation_control = this.getAttribute('navigation-control');
+    if(this.navigation_control === null) this.navigation_control = false;
+
     this.slideshow = this.getAttribute('slideshow');
   }
 
-  // Placeholder functions for future implementation
-  showLayer() {
-    return console.error('FEATURE NOT IMPLEMENTED');
+  showLayer(layer_id){  
+    const visibility = this.map.getLayoutProperty(layer_id, 'visibility');
+    if (typeof visibility !== 'undefined') {
+      if (visibility === 'none') {
+        this.map.setLayoutProperty(layer_id, 'visibility', 'visible');
+      }
+    }
   }
 
-  hideLayer() {
-    return console.error('FEATURE NOT IMPLEMENTED');
+  hideLayer(layer_id){
+    var visibility = this.map.getLayoutProperty(layer_id, 'visibility');
+    if (typeof visibility !== 'undefined') {
+      if (visibility !== 'none') {
+        this.map.setLayoutProperty(layer_id, 'visibility', 'none');
+      }
+    }
   }
 
-  getLayers() {
-    return console.error('FEATURE NOT IMPLEMENTED');
+  getLayer(layer_id){
+    // Get the layer by ID
+    const layer = this.map.getLayer(layer_id);
+
+    // Check if the layer exists
+    if (layer) {
+      return layer;
+    } else {
+      return console.error('Layer not found.');
+    }
+  }
+  getLayers(){
+ 
+    // These Layers are Default.
+    const default_layers = [
+      "background",
+      "satellite",
+      "tunnel-minor-case",
+      "tunnel-street-case",
+      "tunnel-minor-link-case",
+      "tunnel-secondary-tertiary-case",
+      "tunnel-primary-case",
+      "tunnel-major-link-case",
+      "tunnel-motorway-trunk-case",
+      "tunnel-path",
+      "tunnel-steps",
+      "tunnel-pedestrian",
+      "tunnel-minor",
+      "tunnel-minor-link",
+      "tunnel-major-link",
+      "tunnel-street",
+      "tunnel-street-low",
+      "tunnel-secondary-tertiary",
+      "tunnel-primary",
+      "tunnel-motorway-trunk",
+      "road-path",
+      "road-steps",
+      "road-pedestrian",
+      "road-minor-case",
+      "road-street-case",
+      "road-minor-link-case",
+      "road-secondary-tertiary-case",
+      "road-primary-case",
+      "road-major-link-case",
+      "road-motorway-trunk-case",
+      "road-minor",
+      "road-minor-link",
+      "road-major-link",
+      "road-street",
+      "road-street-low",
+      "road-secondary-tertiary",
+      "road-primary",
+      "road-motorway-trunk",
+      "bridge-path",
+      "bridge-steps",
+      "bridge-pedestrian",
+      "bridge-minor-case",
+      "bridge-street-case",
+      "bridge-minor-link-case",
+      "bridge-secondary-tertiary-case",
+      "bridge-primary-case",
+      "bridge-major-link-case",
+      "bridge-motorway-trunk-case",
+      "bridge-minor",
+      "bridge-minor-link",
+      "bridge-major-link",
+      "bridge-street",
+      "bridge-street-low",
+      "bridge-secondary-tertiary",
+      "bridge-primary",
+      "bridge-motorway-trunk",
+      "bridge-major-link-2-case",
+      "bridge-motorway-trunk-2-case",
+      "bridge-major-link-2",
+      "bridge-motorway-trunk-2",
+      "aerialway",
+      "admin-1-boundary-bg",
+      "admin-0-boundary-bg",
+      "admin-1-boundary",
+      "admin-0-boundary",
+      "admin-0-boundary-disputed",
+      "road-label",
+      "road-intersection",
+      "road-number-shield",
+      "road-exit-shield",
+      "path-pedestrian-label",
+      "ferry-aerialway-label",
+      "waterway-label",
+      "natural-line-label",
+      "natural-point-label",
+      "water-line-label",
+      "water-point-label",
+      "poi-label",
+      "transit-label",
+      "airport-label",
+      "settlement-subdivision-label",
+      "settlement-minor-label",
+      "settlement-major-label",
+      "state-label",
+      "country-label",
+      "continent-label"
+    ]
+
+    const layers = this.map.getStyle().layers;
+    let unique_layers = []
+
+    // Iterate over the layers and print their IDs
+    layers.forEach(function(layer) {
+      if(default_layers.indexOf(layer.id) < 0){
+        unique_layers.push(unique_layers);
+      }
+    });
+
+    return unique_layers;
   }
 
-  // Called when the component is added to the DOM
+  getGeoJSON(geoJsonUrl, property){
+    fetch(geoJsonUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then((data) => {
+
+      console.log(data)
+      // Add the GeoJSON layer to the map
+      this.map.addSource('geojson-data', {
+        type: 'geojson',
+        data: data
+      });
+
+      // Add a layer to visualize the GeoJSON data
+      this.map.addLayer({
+        id: 'geojson-layer',
+        type: 'circle',
+        source: 'geojson-data',
+        paint: {
+          'circle-radius': {
+            property: property,
+            type: 'exponential',
+            stops: [
+              [0, 2],
+              [10, 20]
+            ]
+          },
+          'circle-color': '#f00',
+          'circle-opacity': 0.5
+        }
+      });
+
+      this.showLayer('geojson-layer');
+    })
+    .catch(function(error) {
+      console.log('Error fetching GeoJSON:', error);
+    });
+  }
+
   connectedCallback() {
-    ready(() => this.initialize());
+    ready(() => this.initialize())
   }
 
-  // Initializes the map
-  async initialize() {
-    const el = document.createElement('map-container');
-    this.appendChild(el);
-
+  async initialize(){
+    const el = document.createElement('map-container')
+    this.appendChild(el)
     this.map = await new mapboxgl.Map({
-      container: el,
-      style: this.styleurl,
+      container: el, // container ID
+      style: this.styleurl, // style URL
       center: [this.longitude, this.latitude],
       zoom: this.zoom,
       bearing: this.bearing,
       projection: 'globe',
       pitch: this.pitch,
-      interactive: !this.locked,
-    });
+      interactive: !this.locked
+    })
 
     this.initialized = true;
-    this.map.on('load', () => {
-      this.mapLoaded();
-    });
+    this.map.on('load', () => {this.mapLoaded()})
   }
 
-  // Initializes Mapbox geocoder if specified
-  initializeGeoCoder() {
+  initializeGeoCoder(){
     let bbox = this.getAttribute('search-bounds');
-    if (bbox !== null) {
-      bbox = bbox.split(',').map(d => Number(d.trim()));
+    if(bbox !== null){
+      bbox = bbox.split(',').map(d => {
+        return Number(d.trim());
+      });
     }
 
     const geocoder = new MapboxGeocoder({
@@ -98,118 +267,139 @@ class GeoMapComponent extends HTMLElement {
       mapboxgl: mapboxgl,
       zoom: 18,
       marker: false,
-      bbox: bbox,
-      placeholder: 'Search for an Address',
-    });
+      bbox:bbox,
+      placeholder: 'Search for an Address'
+    })
 
-    this.map.addControl(geocoder);
+    this.map.addControl( geocoder )
+   
   }
 
-  // Initializes Mapbox geolocation control if specified
-  initializeGeoLocate() {
+  initializeGeoLocate(){
     const geolocate = new mapboxgl.GeolocateControl({
       showAccuracy: false,
-      showUserLocation: false,
+      showUserLocation: false
     });
 
     this.map.addControl(geolocate);
   }
 
-  // Handles the end of map movement
-  handleMoveEnd(e) {
+  initializeNavigationControl(){
+    const nav_control = new mapboxgl.NavigationControl({
+      visualizePitch: true
+    })
+    this.map.addControl(nav_control);
+  }
+
+  handleMoveEnd(e){
     let coords = this.map.getCenter();
     const bounds = this.map.getBounds();
     const zoom = this.map.getZoom();
     this.handleZoom(zoom);
-
-    // Dispatches an event with map details
     this.dispatchEvent(
-      new CustomEvent('MAP MOVED', {
-        detail: {
-          coords,
-          bounds,
-          zoom,
-        },
-      })
+      new CustomEvent('MAP MOVED', 
+        {
+          detail: {
+            coords,
+            bounds,
+            zoom
+          }
+        }
+      )
     );
   }
 
-  // Handles zoom level and updates CSS classes accordingly
-  handleZoom(zoom = 0) {
+  handleZoom(zoom = 0){
     let mid_zoom_breakpoint = 15;
-    let far_zoom_breakpoint = 10;
+    let far_zoom_breakpoint = 10; 
     const zoom_breakpoints = this.getAttribute('zoom-breakpoints');
-    if (zoom_breakpoints !== null) {
+    if(zoom_breakpoints !== null){
       [mid_zoom_breakpoint, far_zoom_breakpoint] = zoom_breakpoints.split(',').map(n => Number(n));
     }
-    if (zoom < far_zoom_breakpoint) {
-      this.classList.add('far');
-      this.classList.remove('middle');
-      this.classList.remove('near');
-    } else if (zoom >= far_zoom_breakpoint && zoom <= mid_zoom_breakpoint) {
-      this.classList.add('middle');
-      this.classList.remove('far');
-      this.classList.remove('near');
+    if(zoom < far_zoom_breakpoint){
+        this.classList.add('far');
+        this.classList.remove('middle');
+        this.classList.remove('near');
+    } else if(zoom >= far_zoom_breakpoint && zoom <= mid_zoom_breakpoint){
+        this.classList.add('middle')
+        this.classList.remove('far')
+        this.classList.remove('near')
     } else {
-      this.classList.add('near');
-      this.classList.remove('middle');
-      this.classList.remove('far');
+        this.classList.add('near')
+        this.classList.remove('middle')
+        this.classList.remove('far')
     }
   }
 
-  // Displays a popup on the map
-  showPopup(content) {
-    const popup = new mapboxgl.Popup({
-      closeOnClick: false,
+  showPopup(content){
+    const popup = new mapboxgl.Popup({ 
+      closeOnClick: false, 
       closeOnMove: true,
-      offset: {
-        'bottom': [-60, -10],
-      },
+       offset: {
+            'bottom': [-60, -10]
+          },
     })
-      .setLngLat(geo_map.map.getCenter())
-      .setHTML(content)
-      .addTo(this.map);
+    .setLngLat(geo_map.map.getCenter())
+    .setHTML(content)
+    .addTo(this.map)
   }
 
-  // Called when the map has finished loading
-  mapLoaded() {
-    // Checking and initializing Mapbox geocoder
-    if (this.geocoder !== null) {
-      if (typeof MapboxGeocoder === 'undefined') {
+  mapLoaded(){
+
+    if(this.geocoder !== null){   
+      if(typeof(MapboxGeocoder) === 'undefined'){
         this.innerHTML = `If you would like to use the geocoder element, 
         you must include the geocoder plugin in your HTML: 
-        https://docs.mapbox.com/mapbox-gl-js/example/mapbox-gl-geocoder/`;
-        return;
-      }
+        https://docs.mapbox.com/mapbox-gl-js/example/mapbox-gl-geocoder/`
+        return
+      } 
 
       this.initializeGeoCoder();
     }
 
-    // Initializing Mapbox geolocation control
-    this.geolocate_attribute = this.getAttribute('geolocate');
-    if (this.geolocate_attribute !== null) {
+    this.geolocate_attribute = this.getAttribute('geolocate')
+    if(this.geolocate_attribute !== null){
       this.initializeGeoLocate();
     }
 
-    // Event listeners for map interactions
+    if(this.navigation_control){
+      this.initializeNavigationControl();
+    }
+
     this.map.on('moveend', (e) => {
-      this.handleMoveEnd(e);
+      this.handleMoveEnd(e)
     });
 
     this.map.on('click', (e) => {
-      this.map.flyTo({ center: e.lngLat, zoom: 18.5 });
-      [...document.querySelectorAll('geo-map-modal')].forEach((modal) => {
+      this.map.flyTo({center:e.lngLat, zoom:18.5});
+      [...document.querySelectorAll('geo-map-modal')].forEach( modal => {
         modal.hideModal();
-      });
+      })
     });
+
+    const geo_json_component = this.querySelector('geo-json');
+    if(geo_json_component !== null){
+      const src = geo_json_component.getAttribute('src');
+      const variable = geo_json_component.getAttribute('variable');
+      this.getGeoJSON(src, variable);
+    }
+
+
 
     this.style.opacity = 1;
     this.handleZoom(this.zoom);
 
-    // Dispatching a 'loaded' event
-    this.dispatchEvent(new Event('loaded'));
+    /*
+    // This emits an even when the map is loaded. 
+    // you can attach a function to this like
+    //  geo_map.addEventListener('loaded', () => {
+    //    your function here
+    //  });
+    */
+
+    this.dispatchEvent(new Event('loaded'))
   }
 }
 
-// Registering the custom element
-customElements.define('geo-map', GeoMapComponent);
+customElements.define('geo-map', GeoMapComponent)
+
