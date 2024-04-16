97a308c9a3ff5e037ec7a63c2da5b31dce0ab57c
LNSY <lindsey.asterius@gmail.com>
2024-04-16 11:34:15 -0700

    removed modal et al for now

--- src/map-pin.js
+++ src/map-pin.js
@@ -4,19 +4,26 @@ class MapPin extends HTMLElement {
       return {...acc, [name]: this.getAttribute(name)};
     }, {});
     const required_attributes = ['latitude', 'longitude'];
-    this.observedAttributes = requireed_attributes;
+    this.observedAttributes = required_attributes;
 
     required_attributes.forEach(attribute => {
       if(typeof this.attrs[attribute] === 'undefined'){
         return console.error(`The attribute ${attribute} is required;`)
       }
     });
-    let parent = element.parentElement;
-    while (parent && parent.tagName !== 'GEO-MAP') {
-      parent = parent.parentElement;
-    }
+    parent.addEventListener('GEO MAP LOADED', (e) => {
+      console.log('geo-map-loaded')
+      this.initialize();
+    })
+
+  }
+
+  initialize(){
+    const parent = this.parentElement;
+
     this.map = parent.map;
     console.log(this.map)
+
   }
 
   static get observedAttributes() {
