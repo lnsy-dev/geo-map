a6e029191401290a2103dcd396c8a777d428462a
LNSY <lindsey.asterius@gmail.com>
2024-04-16 13:28:55 -0700

    added sidebar

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,12 +1,24 @@
 class MapSidebar extends HTMLElement {
   updateContent(content){
-    const main = this.querySelector('main');
+    let main = this.querySelector('main');
     if(main === null){
-      this.innerHTML = content;
+      main = document.createElement('main');
+      this.appendChild(main);
+      main.innerHTML = content;
     } else {
       main.innerHTML = content;
     }
+    this.classList.add('show');
+  }
+  connectedCallback() {
+   
+    const closeButton = document.createElement('div');
+    closeButton.classList.add('close-button');
+    closeButton.innerHTML = 'x'
+    closeButton.addEventListener('click', () => {
+      this.classList.remove('show');
+    });
+    this.appendChild(closeButton);
   }
 }
-
-customElements.define('map-sidebar', MapSidebar)
+customElements.define('map-sidebar', MapSidebar);
