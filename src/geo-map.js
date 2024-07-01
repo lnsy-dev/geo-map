4fde5c62149b3549599207e187b3ffa2b97fd518
LNSY <lindsey.asterius@gmail.com>
2024-07-01 12:49:06 -0700

    mapbox to maplibre

---
+++ src/geo-map.js
@@ -0,0 +1,74 @@
+import "https://unpkg.com/maplibre-gl@latest/dist/maplibre-gl.js";
+
+import DataroomElement from './vendor/dataroom-element.js';
+import { default_layers }  from './default-layers.js';
+import { ready, getURLValues } from './vendor/helpers.js';
+
+class GeoMap extends DataroomElement {
+  async initialize(){
+
+    // Required Attributes
+    // 
+    const required_attrs = ['style-url'];
+    for(const req_attr of required_attrs){
+      if(!this.attrs[req_attr]){
+        this.innerHTML = `<error>ERROR: Missing ${req_attr} Attribute</error>`;
+        return
+      }
+    }
+
+    // get URL Values
+    const URLvalues = getURLValues();
+
+
+    // Get attributes that describe the map
+    this.latitude = this.getAttribute('latitude');
+    if(this.latitude === null) this.latitude = 0;
+    this.latitude = URLvalues.latitude ? URLvalues.latitude : this.latitude;
+
+    this.longitude = this.getAttribute('longitude');
+    if(this.longitude === null) this.longitude = 0;
+    this.longitude = URLvalues.longitude ? URLvalues.longitude : this.longitude;
+
+    this.zoom = this.getAttribute('zoom');
+    if(this.zoom === null) this.zoom = 1;
+    this.zoom = URLvalues.zoom ? URLvalues.zoom : this.zoom;
+
+    this.bearing = this.getAttribute('bearing');
+    if(this.bearing === null) this.bearing = 0;
+    this.bearing = URLvalues.bearing ? URLvalues.bearing : this.bearing;
+
+    this.pitch = this.getAttribute('pitch');
+    if(this.pitch === null) this.pitch = 0;
+    this.pitch = URLvalues.pitch ? URLvalues.pitch : this.pitch;
+
+    this.locked = this.getAttribute('locked');
+    if(this.locked === null){
+      this.locked = false;
+    } else {
+      this.locked = true;
+    };
+
+    this.navigation_control = this.getAttribute('navigation-control');
+    if(this.navigation_control === null) this.navigation_control = false;
+
+
+    this.render();
+  }
+
+  async render(){
+
+    this.innerHTML = '<map-container id="map"></map-container>';
+
+    this.map = new maplibregl.Map({
+        container: 'map', // container id
+        style: this.attrs["style-url"], // style URL
+        center: [0, 0], // starting position [lng, lat]
+        zoom: 1 // starting zoom
+    });
+
+
+  }
+}
+
+customElements.define('geo-map', GeoMap)
