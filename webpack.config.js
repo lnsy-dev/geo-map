2e76466fd0cb8771a4460abfa0b45012b5e65088
LNSY <lindsey.asterius@gmail.com>
2024-07-15 07:44:16 -0700

    removed index.html from webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -30,28 +30,6 @@ module.exports = {
         use:['style-loader','css-loader']
      }
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      templateContent: () => {
-        const index_path = path.resolve(__dirname, 'index.html');
-        try {
-          // Use synchronous readFileSync instead of asynchronous readFile
-          const data = fs.readFileSync(index_path, 'utf8');
-          // Replace the target string
-          return data.replace(
-            /<link rel="stylesheet" href="\.\/styles\/index\.css">[\s\S]*?<script src="\.\/src\/index\.js" type="module"><\/script>/,
-            `<script src="./${file_name}" type="module"></script>`
-          );
-        } catch (error) {
-          console.error('Error reading file:', error);
-          throw error;
-        }
-      },
-      filename: 'index.html', // Output file name
-      inject: 'body', // Inject script tag into the body
-      scriptLoading: 'blocking', // Ensure script tag is loaded in the correct order
-    })
-  ]
+  }
 };
 
